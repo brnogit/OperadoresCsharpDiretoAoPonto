927a2348567880eb13cbe19af2f2317e0edfa643
bmotadev <bmotadev@gmail.com>
2022-08-16 17:04:21 -0300

    Operadores Unarios

--- OperadoresCsharp/Program.cs
+++ OperadoresCsharp/Program.cs
@@ -10,6 +10,26 @@ namespace OperadoresCsharp
     {
         static void Main(string[] args)
         {
+            #region Operadores Aritméticos
+            // Unários ++, --, + e -
+            
+            //sufixo que vem depois
+            int numeroOperador = 4;
+
+            Console.WriteLine(numeroOperador++);// 4
+            Console.WriteLine(numeroOperador--);// 5
+            // prefixo que vem antes
+            Console.WriteLine(++numeroOperador); // 5
+            Console.WriteLine(--numeroOperador);// 4
+
+            Console.WriteLine(numeroOperador);
+            Console.WriteLine(-numeroOperador);
+            Console.WriteLine(-(-numeroOperador));
+
+            // Binários * / + -
+            #endregion
+
+            Console.Read();
         }
     }
 }
